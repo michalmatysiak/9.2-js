b3d353c73b90bd2c892a9c5b1b06780cc647973b
Michał Matysiak <michalmatysiak02@gmail.com>
2018-05-09 23:08:14 +0200

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-var maleNames = ['Michał', 'Adam', 'Alojzy', 'Tadeusz', 'Roman'];
-var femaleNames = ['Eliza', 'Elżbieta', 'Mieczysława', 'Genowefa', 'Weronika'];
+var maleNames = ["Michał", "Adam", "Alojzy", "Tadeusz", "Roman"];
+var femaleNames = ["Eliza", "Elżbieta", "Mieczysława", "Genowefa", "Weronika"];
 var allNames = maleNames.concat(femaleNames);
 console.log(allNames);
 
-var newName = 'Marian';
-var allNamesMarian = allNames.concat(newName);
+var newName = "Eliza";
 
 if (allNames.indexOf(newName) === -1) {
-  var x = allNames.push(allNames);
-} 
-else {
-  var x = 'Name duplicated';
+	var x = allNames.push(newName);
+} else {
+	var x = "Name duplicated";
 }
-
-console.log(allNamesMarian);
+console.log(x);
+console.log(allNames);
